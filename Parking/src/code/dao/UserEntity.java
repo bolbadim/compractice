1708274ab723e199f17472a61732c6e2830dcb69
Dmitry Bolbat <bolbadim@gmail.com>
2020-04-03 00:27:39 +0300

    second lab with new classes

--- Parking/src/code/dao/UserEntity.java
+++ Parking/src/code/dao/UserEntity.java
@@ -1,22 +1,26 @@
-<<<<<<< HEAD
 package code.dao;
 
+
+import java.util.Arrays;
+import java.util.Objects;
 
 public class UserEntity {
     private String username;
     private String password;
     private boolean isAdmin;
+    private CarEntity[] cars;
 
     public UserEntity(){
-        this.username = null;
-        this.password = null;
+        this.username = "";
+        this.password = "";
         this.isAdmin = false;
     }
 
-    public UserEntity(String username, String password, boolean isAdmin){
+    public UserEntity(String username, String password, boolean isAdmin, CarEntity[] cars) throws NullPointerException {
         this.username = username;
         this.isAdmin = isAdmin;
         this.password = password;
+        this.cars = cars;
     }
 
     public String getUsername() {
@@ -29,55 +33,7 @@ public class UserEntity {
         return isAdmin;
     }
 
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public void setPassword(String password) { this.password = password;}
-
-    public void setAdmin(boolean admin) {
-        isAdmin = admin;
-    }
-
-    @Override
-    public String toString() {
-        final StringBuilder sb = new StringBuilder("");
-        sb.append("\"username\" : \"").append(username).append("\",\n");
-        sb.append("\"password\" : \"").append(password).append("\",\n");
-        sb.append("\"isAdmin\" : ").append(isAdmin);
-        sb.append('\n');
-        return sb.toString();
-    }
-=======
-package code.dao;
-
-
-public class UserEntity {
-    private String username;
-    private String password;
-    private boolean isAdmin;
-
-    public UserEntity(){
-        this.username = null;
-        this.password = null;
-        this.isAdmin = false;
-    }
-
-    public UserEntity(String username, String password, boolean isAdmin){
-        this.username = username;
-        this.isAdmin = isAdmin;
-        this.password = password;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public String getPassword() { return password; }
-
-    public boolean isAdmin() {
-        return isAdmin;
-    }
+    public CarEntity[] getCars() { return cars;}
 
     public void setUsername(String username) {
         this.username = username;
@@ -89,14 +45,42 @@ public class UserEntity {
         isAdmin = admin;
     }
 
+    public void setCars(CarEntity[] cars)  {
+        this.cars = cars;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        UserEntity that = (UserEntity) o;
+        return isAdmin == that.isAdmin &&
+                Objects.equals(username, that.username) &&
+                Objects.equals(password, that.password) &&
+                Arrays.equals(cars, that.cars);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = Objects.hash(username, password, isAdmin);
+        result = 31 * result + Arrays.hashCode(cars);
+        return result;
+    }
+
     @Override
     public String toString() {
         final StringBuilder sb = new StringBuilder("");
         sb.append("\"username\" : \"").append(username).append("\",\n");
         sb.append("\"password\" : \"").append(password).append("\",\n");
-        sb.append("\"isAdmin\" : ").append(isAdmin);
-        sb.append('\n');
+        sb.append("\"isAdmin\" : ").append(isAdmin).append(",\n");
+        sb.append("\"cars\" : [\n");
+        for (int i=0; i<cars.length; i++){
+            sb.append("{\n");
+            sb.append(cars[i].toString());
+            sb.append("},\n");
+        }
+        sb.deleteCharAt(sb.lastIndexOf(","));
+        sb.append("]\n");
         return sb.toString();
     }
->>>>>>> d305a227df5d2b5b2393570a095528e85b9c0d3d
 }
--- Parking/src/code/service/UserMapper.java
+++ Parking/src/code/service/UserMapper.java
@@ -1,6 +1,6 @@
-<<<<<<< HEAD
 package code.service;
 
+import code.dao.CarEntity;
 import code.dao.UserEntity;
 import code.domain.User;
 
@@ -10,22 +10,7 @@ public class UserMapper {
         String username = userEntity.getUsername();
         String password = userEntity.getPassword();
         boolean isAdmin = userEntity.isAdmin();
-        return new User(username, password, isAdmin);
+        CarEntity[] cars = userEntity.getCars();
+        return new User(username, password, isAdmin, cars);
     }
 }
-=======
-package code.service;
-
-import code.dao.UserEntity;
-import code.domain.User;
-
-public class UserMapper {
-
-    public User mapToUser(UserEntity userEntity) throws Exception {
-        String username = userEntity.getUsername();
-        String password = userEntity.getPassword();
-        boolean isAdmin = userEntity.isAdmin();
-        return new User(username, password, isAdmin);
-    }
-}
->>>>>>> d305a227df5d2b5b2393570a095528e85b9c0d3d
